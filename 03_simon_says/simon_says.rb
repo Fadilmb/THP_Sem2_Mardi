757dc730e0eede70c64d30ad63038c52c4abe5ed
Fadilmb <fadil.boodoo@etu.upmc.fr>
2017-07-12 04:24:41 +0200

    Done

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,46 @@
-#write your code here
+def echo(word)
+	word
+end
+
+def shout(word)
+	word.upcase
+end
+
+#def self.repeat (word)
+	#"#{word} #{word}"
+#end
+
+def repeat(word, number=2)
+	repeat = "#{word}"
+	for i in (2..number)
+		repeat += " #{word}"
+	end
+	repeat
+end
+
+def start_of_word(word, number=1)
+	all = ""
+	for i in (1..number)
+		all += word.split("")[i-1]
+	end
+	all
+end
+
+def first_word(sentence)
+	sentence.split(" ")[0]
+end
+
+def titleize(word)
+	total = word.split(" ").map!{|x|
+		if x == "and" || x == "over" || x== "the"
+			x = x
+		else	
+			x.capitalize!
+		end
+	}
+	title = total[0].capitalize
+	for i in (1..total.length-1)
+		title += (" " + total[i])
+	end
+	title
+end
